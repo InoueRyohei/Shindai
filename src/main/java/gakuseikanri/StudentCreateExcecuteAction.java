d8e0c209d662f9179cbfcd787e57c886793eeb0d
r_inoue <r_inoue@LAPTOP-16HH3G54>
2024-05-07 11:50:55 +0900

    5/7(2)

--- src/main/java/gakuseikanri/StudentCreateExcecuteAction.java
+++ src/main/java/gakuseikanri/StudentCreateExcecuteAction.java
@@ -3,9 +3,11 @@ package gakuseikanri;
 import java.util.List;
 
 import bean.Student;
+import bean.Teacher;
 import dao.StudentDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 
@@ -13,17 +15,24 @@ public class StudentCreateExcecuteAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
-	
+		
+		
+		HttpSession session=request.getSession();
 		String name=request.getParameter("name");
 		String no=request.getParameter("no");
 		Integer ent_year=Integer.parseInt(request.getParameter("ent_year"));
 		Integer class_num=Integer.parseInt(request.getParameter("class_num"));
+		boolean is_attend=true;
+		Teacher teacher=(Teacher)session.getAttribute("teacher");
+		String school_cd=teacher.getSchool_cd();
 		
 		Student p=new Student();
 		p.setEnt_year(ent_year);
 		p.setNo(no);
 		p.setName(name);
-		p.setClass_num(class_num);		
+		p.setClass_num(class_num);
+		p.setIs_attend(is_attend);
+		p.setSchool_cd(school_cd);
 		StudentDAO dao=new StudentDAO();
 		dao.insert(p);
 
